7e7c9b01a6e121e7ee2244e3f24c6481534e2a3e
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-08-05 12:29:45 -0300

    update Java

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author Camila
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
