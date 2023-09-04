f7e50672647ca90b52242b701541c5c0b320e748
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-09-04 20:16:55 -0300

    update exercicios java 23/08

---
+++ SENAI/3_INTRO_JAVA/Aula23_08/src/aula23_08/Exe16.java
@@ -0,0 +1,24 @@
+
+package aula23_08;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author camila_alves3
+ */
+public class Exe16 {
+    public static void main(String[] args) {
+        //Entrada de dados
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Informe um nome: ");
+        String nome = entrada.nextLine();
+        
+        // Saída de dados
+        System.out.printf("Welcome to the world, %s!\n", nome);
+        
+        entrada.close();        
+    }
+    
+}
