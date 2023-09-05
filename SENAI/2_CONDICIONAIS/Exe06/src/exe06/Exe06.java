be3ede3ce0433201af60bf65793d5131e1da2d86
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-09-05 16:58:57 -0300

    update camelCase

--- SENAI/2_CONDICIONAIS/Exe06/src/exe06/Exe06.java
+++ SENAI/2_CONDICIONAIS/Exe06/src/exe06/Exe06.java
@@ -13,25 +13,25 @@ public class Exe06 {
         Scanner entrada = new Scanner(System.in);
         
         System.out.println("Informe a quantidade de horas trabalhadas no mês: ");
-        float qtd_horas = entrada.nextFloat();
+        float qtdHoras = entrada.nextFloat();
         
         System.out.println("Informe o valor hora do colaborador: ");
-        float valor_hora = entrada.nextFloat();
+        float valorHora = entrada.nextFloat();
         
         // Processamento de dados
-        float resto = qtd_horas % 160;
-        float hora_extra = (float) (resto * valor_hora * (1.50));
+        float resto = qtdHoras % 160;
+        float hora_extra = (float) (resto * valorHora * (1.50));
         
-        if (qtd_horas > 160) {
+        if (qtdHoras > 160) {
             float salario;
             salario = 0;
-            salario = hora_extra + (qtd_horas - resto) * valor_hora;
-            System.out.printf("Total de horas trabalhadas no mês: %s, sendo %shs extras. Salário do mês: R$%s.\n", qtd_horas, resto, salario);
+            salario = hora_extra + (qtdHoras - resto) * valorHora;
+            System.out.printf("Total de horas trabalhadas no mês: %s, sendo %shs extras. Salário do mês: R$%s.\n", qtdHoras, resto, salario);
         }
         else {
             float salario;
-            salario = qtd_horas * valor_hora;
-            System.out.printf("Total de horas trabalhadas no mês: %s. Salário do mês: R$%s.\n", qtd_horas, salario);
+            salario = qtdHoras * valorHora;
+            System.out.printf("Total de horas trabalhadas no mês: %s. Salário do mês: R$%s.\n", qtdHoras, salario);
         }   
         
     }
--- SENAI/2_CONDICIONAIS/PrepProva01/src/prepprova01/Exe01.java
+++ SENAI/2_CONDICIONAIS/PrepProva01/src/prepprova01/Exe01.java
@@ -19,28 +19,28 @@ public class Exe01 {
         int publico = entrada.nextInt();
         
         // Processamento de dados
-        float qtd_popular = publico * 0.10f;
-        float ing_popular = 5.00f * qtd_popular;
+        float qtdPopular = publico * 0.10f;
+        float ingPopular = 5.00f * qtdPopular;
         
-        float qtd_geral = publico * 0.50f;
-        float ing_geral = 10.00f * qtd_geral;        
+        float qtdGeral = publico * 0.50f;
+        float ingGeral = 10.00f * qtdGeral;        
         
-        float qtd_arquibancada =  publico * 0.30f;
-        float ing_arquibancada = 20.00f * qtd_arquibancada;
+        float qtdArquibancada =  publico * 0.30f;
+        float ingArquibancada = 20.00f * qtdArquibancada;
         
-        float qtd_cadeiras = publico * 0.10f;
-        float ing_cadeiras = 30.00f * qtd_cadeiras;
+        float qtdCadeiras = publico * 0.10f;
+        float ingCadeiras = 30.00f * qtdCadeiras;
 
-        float valor_total = (ing_cadeiras + ing_arquibancada + ing_cadeiras + ing_popular);
+        float valor_total = (ingCadeiras + ingArquibancada + ingGeral + ingPopular);
  
         // Saída de dados:
         System.out.println("Informações da partida de futebol: ");
         System.out.printf(" - Público total da partida: %s pessoas.\n", publico);
         System.out.printf(" - Valor total da partida: R$%.2f.\n", valor_total);
-        System.out.printf(" - Ingresso popular: %.0f de pessoas R$%.2f.\n", qtd_popular, ing_popular);
-        System.out.printf(" - Ingresso geral: %.0f de pessoas R$%.2f.\n", qtd_geral, ing_geral);
-        System.out.printf(" - Ingresso arquibancada: %.0f de pessoas R$%.2f.\n", qtd_arquibancada, ing_arquibancada);
-        System.out.printf(" - Ingresso cadeiras: %.0f de pessoas R$%.2f.\n", qtd_cadeiras, ing_cadeiras);
+        System.out.printf(" - Ingresso popular: %.0f de pessoas R$%.2f.\n", qtdPopular, ingPopular);
+        System.out.printf(" - Ingresso geral: %.0f de pessoas R$%.2f.\n", qtdGeral, ingGeral);
+        System.out.printf(" - Ingresso arquibancada: %.0f de pessoas R$%.2f.\n", qtdArquibancada, ingArquibancada);
+        System.out.printf(" - Ingresso cadeiras: %.0f de pessoas R$%.2f.\n", qtdCadeiras, ingCadeiras);
         
         entrada.close();      
         
