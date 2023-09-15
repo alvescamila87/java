e799843e3075266b5b3dc3c2ea2198886bb36b94
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-09-15 17:43:06 -0300

    update exercicio VETOR

---
+++ SENAI/3_INTRO_JAVA/Aula04_Vetores/src/aula04_vetores/Vetores_Exe01_numerosInteiros.java
@@ -0,0 +1,44 @@
+package aula04_vetores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Camila
+ */
+public class Vetores_Exe01_numerosInteiros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        
+        int[] vetorNumeros = new int[3];
+        
+        // inserir valores no array
+        for (int i = 0; i < vetorNumeros.length; i++) {
+            
+            System.out.printf("Informe o %sº número inteiro: \n", i+1);
+            vetorNumeros[i] = entrada.nextInt();            
+        }
+        
+        // verificar maior valor do array
+        int maiorValor = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < vetorNumeros.length; i++) {
+            if (vetorNumeros.length == 1) {
+                maiorValor = vetorNumeros[i]; 
+                System.out.printf("Maior valor informado no vetor: '%s' na posição %s. \n",maiorValor, i);
+            } else {
+                if (vetorNumeros[i] > maiorValor) {
+                    maiorValor = vetorNumeros[i];
+                    System.out.printf("Maior valor informado no vetor: '%s' na posição %s. \n",maiorValor, i);
+                }
+            }
+        }
+
+        
+    }
+    
+}
