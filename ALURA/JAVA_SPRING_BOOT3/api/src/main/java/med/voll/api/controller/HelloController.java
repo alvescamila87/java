6dcf25f5f3b87e991f6710bf0e9069a32e616211
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-09-27 20:19:33 -0300

    update SPRING BOOT

---
+++ ALURA/JAVA_SPRING_BOOT3/api/src/main/java/med/voll/api/controller/HelloController.java
@@ -0,0 +1,22 @@
+
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Camila
+ */
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    
+    @GetMapping
+    public String helloWorld() {
+        return "Hello world Spring!";
+    }
+    
+}
