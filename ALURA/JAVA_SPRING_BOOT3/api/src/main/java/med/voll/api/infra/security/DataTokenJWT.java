059ce2876cc69b4c0d050e7f2fb760733b96f79a
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-10-04 17:00:19 -0300

    update create filter

--- ALURA/JAVA_SPRING_BOOT3/api/src/main/java/med/voll/api/infra/security/DataTokenJWT.java
+++ ALURA/JAVA_SPRING_BOOT3/api/src/main/java/med/voll/api/infra/security/DataTokenJWT.java
@@ -1,4 +1,6 @@
 package med.voll.api.infra.security;
 
 public record DataTokenJWT(String token) {
+    public static class securityFilter {
+    }
 }
---
+++ ALURA/JAVA_SPRING_BOOT3/api/src/main/java/med/voll/api/infra/security/securityFilter.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class securityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        System.out.println("It's calling filter");
+        
+        var tokenJWT = retrieveToken(request);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String retrieveToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token did not send in Authorization header.");
+        }
+    }
+}
