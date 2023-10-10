a761e3e61c91e7fc4de778e97ead0c33442e19ab
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-10-10 15:47:52 -0300

    update springboot: 2. integração com banco de dados

---
+++ ROCKETSEAT/todolist/src/main/java/br/com/camilaalves/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.camilaalves.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
--- ROCKETSEAT/todolist/src/main/java/br/com/camilaalves/todolist/user/UserController.java
+++ ROCKETSEAT/todolist/src/main/java/br/com/camilaalves/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.camilaalves.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        //System.out.println(userModel.getUsername());
+
+        // erro de usuário quando:
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            //System.out.println("Usuário já cadastrado!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado.");
+        }
+
+        // usuário cadastrado com sucesso:
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
