15574357ebbdd545a07d60851982222e7cd06d7c
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-10-11 18:05:29 -0300

    update 3. implementando segurança nos dados do usuário

--- ROCKETSEAT/todolist/src/main/java/br/com/camilaalves/todolist/filter/FilterTaskAuth.java
+++ ROCKETSEAT/todolist/src/main/java/br/com/camilaalves/todolist/filter/FilterTaskAuth.java
@@ -1,17 +1,23 @@
 package br.com.camilaalves.todolist.filter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.camilaalves.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -20,18 +26,41 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         //Pegar a autenticação (usuário e senha)
         var authorization = request.getHeader("Authorization");
 
-        var user_password = authorization.substring("Basic".length()).trim();
-        System.out.println("Authorization");
-        System.out.println(user_password);
+        var authEncoded = authorization.substring("Basic".length()).trim();
 
+        byte[] authDecoded= Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecoded);
+        System.out.println("Authorization");
+        System.out.println(authString);
+
+        //["silvajoao", "123456"]
+        String[] credencials = authString.split(":");
+        String username = credencials[0];
+        String password = credencials[1];
+        System.out.println("Credencials");
+        System.out.println(username);
+        System.out.println(password);
 
         // Validar usuário
+        var user = this.userRepository.findByUsername(username);
 
-        // Validar senha
+        if(user == null) {
+            response.sendError(401);
+        } else {
 
-        // Rota happy flow
+            // Validar senha
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
+            if (passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
 
+            // Rota happy flow
+
+        }
     }
 }
 
