91c6e071e794555e62c491451032a2e5198b419e
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-10-18 21:44:34 -0300

    update exercicios vetor java

---
+++ SENAI/3_INTRO_JAVA/Aula07_Vetores/src/aula07_vetores/Ex03_vetor_numerosPrimos.java
@@ -0,0 +1,36 @@
+
+package aula07_vetores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author camila_alves3
+ */
+public class Ex03_vetor_numerosPrimos {
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner (System.in);
+        
+        int[] vetorNumeros = new int[5];
+        
+        // entrada
+        for (int i = 0; i < vetorNumeros.length; i++) { 
+            
+            System.out.printf("Informe o %sº número inteiro: ", i + 1);
+            vetorNumeros[i] = entrada.nextInt();
+            
+        }
+        
+        // impressão se é primo
+        for (int i = 0; i < vetorNumeros.length; i++) {
+            if (vetorNumeros[i] % 2 != 0 && vetorNumeros[i] % 3 != 0 || vetorNumeros[i] == 2 || vetorNumeros[i] == 3) {
+                System.out.printf("É primo: %s.\n", vetorNumeros[i]); 
+            }               
+            
+        }       
+                
+    }
+    
+}
---
+++ SENAI/3_INTRO_JAVA/Aula07_Vetores/src/aula07_vetores/Ex06_vetor_produtoSoma.java
@@ -0,0 +1,19 @@
+
+package aula07_vetores;
+
+import java.util.Scanner;
+
+
+public class Ex06_vetor_produtoSoma {
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner (System.in);
+        
+        int[] vetorNumeros = new int[10];
+        
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+}
