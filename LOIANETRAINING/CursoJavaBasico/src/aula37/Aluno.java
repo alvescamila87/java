34c27ea57214d1264d41250342c0410524d7ace2
Camila <alvescamila87@gmail.com>
2023-11-25 11:44:08 -0300

    aula heranca

---
+++ LOIANETRAINING/CursoJavaBasico/src/aula37/Aluno.java
@@ -0,0 +1,67 @@
+package aula37;
+
+public class Aluno {
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+    private String curso;
+    private double[] notas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia() {
+        return 0;
+    }
+
+    public boolean verificarAprovacao() {
+        return true;
+    }
+}
---
+++ LOIANETRAINING/CursoJavaBasico/src/aula37/Professor.java
@@ -0,0 +1,72 @@
+package aula37;
+
+public class Professor {
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+    private String departamento;
+    private String nomeCurso;
+    private double salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double calcularSalarioLiquido() {
+        return 0;  
+    }
+}
