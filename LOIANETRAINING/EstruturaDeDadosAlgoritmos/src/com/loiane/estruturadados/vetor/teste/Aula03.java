a80aa42352c828f92a684d0c51389c442b754ddc
Camila <alvescamila87@gmail.com>
2023-12-19 14:57:41 -0300

    estrutura de dados: vetor aula 02 e 03

---
+++ LOIANETRAINING/EstruturaDeDadosAlgoritmos/src/com/loiane/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,15 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+import com.sun.tools.javac.Main;
+
+public class Aula03 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+
+        vetor.adicionar("elemento 1");
+        vetor.adicionar("elemento 2");
+    }
+}
