543a4e7d8273465ce40a751ef817c8c8197de9e4
Camila <alvescamila87@gmail.com>
2024-03-21 14:36:34 -0300

    streams API: reduce method

--- ALGAWORKS/banco/src/com/algaworks/banco/app/PrincipalStreamsAPI.java
+++ ALGAWORKS/banco/src/com/algaworks/banco/app/PrincipalStreamsAPI.java
@@ -2,8 +2,13 @@ package com.algaworks.banco.app;
 
 import com.algaworks.banco.modelo.Banco;
 import com.algaworks.banco.modelo.Conta;
+import com.algaworks.banco.modelo.Pessoa;
 
 import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PrincipalStreamsAPI {
@@ -12,15 +17,57 @@ public class PrincipalStreamsAPI {
 
         Banco banco = new Banco();
 
-        // api para trabalhar com operações em massa em uma sequencia de elementos: streams API
+        // contiguração de pipeline
+        /*
+        // forma 1 - contas
+        // api para trabalhar com operações em massa em uma sequência de elementos: streams API
         Stream<Conta> stream = banco.getContas().stream();
 
-        // uso de filter da interface predicate
+        // uso de filter da interface predicate: filtrar elementos da condição lambda
         Stream<Conta> stream2 = stream.filter(conta -> conta.getSaldo().compareTo(new BigDecimal("130")) > 0);
 
-        stream2.forEach(conta -> {
+        // filtrar contas que o número seja maior que 300
+        Stream<Conta> stream3 = stream2.filter(conta -> conta.getNumero() > 300);
+
+        // operação terminal para chamar a pipeline lazy do stream em cadeia
+        stream3.forEach(conta -> {
             //conta.depositar(new BigDecimal("10"));
             System.out.println(conta.getAgencia() + "/" + conta.getNumero() + " = " + conta.getSaldo());
         });
+        */
+
+        // forma 2 - contas
+        // forma enxuta do mesmo código acima de pipeline:
+        banco.getContas().stream()
+                .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
+                .filter(conta -> conta.getNumero() > 200)
+                .sorted(Comparator.comparingInt(Conta::getNumero))
+                .forEach(conta -> {
+                    System.out.println(conta.getAgencia() + "/" + conta.getNumero() + " = " + conta.getSaldo());
+                });
+
+        // forma 3 - titulares das contas
+        banco.getContas().stream()
+                .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
+                .filter(conta -> conta.getNumero() > 200)
+                .map(Conta::getTitular)
+                .distinct()
+                .forEach(System.out::println);
+
+        // Operação de redução - saldo total de todas as contas que o banco possui
+        BigDecimal saldoTotal = banco.getContas().stream()
+                .map(Conta::getSaldo)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println(saldoTotal);
+
+        // Operação de terminal - Transformar uma lista de pessoas titulares
+        List<Pessoa> titulares = banco.getContas().stream()
+                .map(Conta::getTitular)
+                .distinct()
+                        .toList();
+        //        .collect(Collectors.toList());
+
+        System.out.println(titulares);
+
     }
 }
